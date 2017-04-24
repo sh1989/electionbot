3f145310763d6133ca8867f4c34290dc2a8027ae
Sam Hogarth <dev@samhogy.co.uk>
2017-04-24 14:50:24 +0100

    ElectionBot can now respond to a channel or chat

--- electionbot.js
+++ electionbot.js
@@ -8,28 +8,29 @@ class ElectionBot extends Bot {
     super(settings);
     this.settings = settings;
     this.user;
-    this.channel;
     this.on('start', this._onStart);
     this.on('message', this._onMessage);
   }
 
   _onStart() {
     this.user = this.users.filter(user => user.name === this.settings.name)[0];
-    this.channel = this.groups.filter(group => group.name === 'lolatics')[0];
-
     console.log('started!');
   }
 
   _onMessage(message) {
     if (this._isFromSomeoneElse(message)
       && this._isMessage(message)
-      && this._isInLoliticsChannel(message)
       && this._mentionsMe(message)) {
-        console.log('== Handling Query');
-        this._sendTypingResponse();
+        this._sendTypingResponse(message.channel);
 
-        westminster.getMessage()
-          .then(this._postMessage, this._postError);
+        if (message.text.toLowerCase().indexOf('westminster') >= 0) {
+          westminster.getMessage()
+            .then(
+              response => { this._postMessage(message.channel, response); },
+              () => { this._postError(message.channel); });
+        } else {
+          this._postDontKnow(message.channel);
+        }
     }
   }
 
@@ -37,29 +38,33 @@ class ElectionBot extends Bot {
     return message.user !== this.user.id;
   }
 
-  _isInLoliticsChannel(message) {
-      return message.channel === this.channel.id;
-  }
-
   _isMessage(message) {
       return message.type === 'message'
         && Boolean(message.text)
   }
 
-  _sendTypingResponse() {
-    this.ws.send(JSON.stringify({ type: 'typing', channel: this.channel.id }));
+  _sendTypingResponse(channel) {
+    this.ws.send(JSON.stringify({ type: 'typing', channel: channel }));
   }
 
   _mentionsMe(message) {
     return message.text.indexOf(`<@${this.user.id}>`) > -1;
   }
 
-  _postMessage(message) {
-    this.postMessageToGroup(this.channel.name, message, { as_user: true });
+  _postMessage(channel, message) {
+    this.postMessage(channel, message, { as_user: true });
   }
 
-  _postError() {
-    this._postMessage(':exclamation: Whoops! Failed to get data');
+  _postError(channel) {
+    this._postMessage(channel, ':exclamation: Whoops! Failed to get data');
+  }
+
+  _postDontKnow(channel) {
+    this._postmessage(
+      channel,
+      ':question: Sorry, I don\'t understand. Try again with one of these keywords:\n' +
+      '*westminster* - For Westminster Polling Data'
+    );
   }
 }
 
