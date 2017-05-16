fb7b33c19481dab3ed2f60f74b409181b42d7d34
Sam Hogarth <dev@samhogy.co.uk>
2017-05-16 07:49:35 +0100

    Britain Elects changed their formatting again

--- __tests__/westminster.test.js
+++ __tests__/westminster.test.js
@@ -4,15 +4,15 @@ const tableScraper = require('table-scraper');
 
 beforeAll(() => {
   tableScraper.setMock([[
-    { 'Con': 48, 'Lab': 24, 'LD': 12, 'UKIP': 7, 'Grn': 2 },
-    { 'Con': 46, 'Lab': 25, 'LD': 11, 'UKIP': 8, 'Grn': 3 },
-    { 'Con': 44, 'Lab': 26, 'LD': 10, 'UKIP': 11, 'Grn': 4 },
-    { 'Con': 44, 'Lab': 23, 'LD': 12, 'UKIP': 10, 'Grn': 4 },
-    { 'Con': 46, 'Lab': 25, 'LD': 11, 'UKIP': 9, 'Grn': 4 },
-    { 'Con': 38, 'Lab': 29, 'LD': 7, 'UKIP': 14, 'Grn': 5 },
-    { 'Con': 42, 'Lab': 25, 'LD': 11, 'UKIP': 11, 'Grn': 3 },
-    { 'Con': 43, 'Lab': 25, 'LD': 11, 'UKIP': 11, 'Grn': 4 },
-    { 'Con': 43, 'Lab': 25, 'LD': 11, 'UKIP': 10, 'Grn': 2 },
+    { 'Con': 48, 'Lab': 24, 'LDem': 12, 'UKIP': 7, 'Grn': 2 },
+    { 'Con': 46, 'Lab': 25, 'LDem': 11, 'UKIP': 8, 'Grn': 3 },
+    { 'Con': 44, 'Lab': 26, 'LDem': 10, 'UKIP': 11, 'Grn': 4 },
+    { 'Con': 44, 'Lab': 23, 'LDem': 12, 'UKIP': 10, 'Grn': 4 },
+    { 'Con': 46, 'Lab': 25, 'LDem': 11, 'UKIP': 9, 'Grn': 4 },
+    { 'Con': 38, 'Lab': 29, 'LDem': 7, 'UKIP': 14, 'Grn': 5 },
+    { 'Con': 42, 'Lab': 25, 'LDem': 11, 'UKIP': 11, 'Grn': 3 },
+    { 'Con': 43, 'Lab': 25, 'LDem': 11, 'UKIP': 11, 'Grn': 4 },
+    { 'Con': 43, 'Lab': 25, 'LDem': 11, 'UKIP': 10, 'Grn': 2 },
   ]]);
 });
 
@@ -23,14 +23,14 @@ test('parses polling table', () => {
         data: {
           con: "44",
           lab: "25",
-          ld: "11",
+          ldem: "11",
           ukip: "10",
           grn: "4"
         },
         diff: {
           con: "1",
           lab: "0",
-          ld: "0",
+          ldem: "0",
           ukip: "-1",
           grn: "0"
         },
